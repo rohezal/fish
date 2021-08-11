ccbdbfb579a1a29aa02100524928c7f0c1ecdf1e
rohezal <you@example.com>
2021-08-11 13:11:26 +0200

    fixed tabs and spaces intends. now all should be spaces

--- map_lightsheet_volume_over_time_to_z_brain.py
+++ map_lightsheet_volume_over_time_to_z_brain.py
@@ -40,60 +40,60 @@ for folder, subfolders, files in os.walk(r'/Volumes/User-Data/Armin/new_registra
 
                 t_size = f_hdf5_motion_aligned["TZYX"].shape[0]  # number of time steps in that file
 
-				# Create a new hdf5 file with same number of time steps
-				filepath_output = Path(os.sep.join([folder, 'zbrain_registered', 'over_time', f'{stack_name}_registered.hdf5']))
-				f_hdf5_z_brain_registered = h5py.File(filepath_output, 'w')
-				dset = f_hdf5_z_brain_registered.create_dataset("TZYX", (t_size, 138, 1406, 621), dtype='uint8')
+                # Create a new hdf5 file with same number of time steps
+                filepath_output = Path(os.sep.join([folder, 'zbrain_registered', 'over_time', f'{stack_name}_registered.hdf5']))
+                f_hdf5_z_brain_registered = h5py.File(filepath_output, 'w')
+                dset = f_hdf5_z_brain_registered.create_dataset("TZYX", (t_size, 138, 1406, 621), dtype='uint8')
 
-				# convert each time point in the hdf5 file to XYZ and to a temporary nrrd file
-				for t in range(t_size):
-					print(f'\ntime step {t}')
+                # convert each time point in the hdf5 file to XYZ and to a temporary nrrd file
+                for t in range(t_size):
+                    print(f'\ntime step {t}')
 
-					start = time.time()
-					nparray = np.array(f_hdf5_motion_aligned["TZYX"][t], dtype=np.uint8)
-					print("Loading \"stack\" Timestep: " + str(time.time()-start))
-					start = time.time()
-					stack = nparray.transpose(2, 1, 0)
-					print("Transposing nparray to stack: " + str(time.time()-start))
+                    start = time.time()
+                    nparray = np.array(f_hdf5_motion_aligned["TZYX"][t], dtype=np.uint8)
+                    print("Loading \"stack\" Timestep: " + str(time.time()-start))
+                    start = time.time()
+                    stack = nparray.transpose(2, 1, 0)
+                    print("Transposing nparray to stack: " + str(time.time()-start))
 
-					options = {'type': 'uint8',
-							   'encoding': 'raw',
-							   'endian': 'big',
-							   'dimension': 3,
-							   'sizes': stack.shape,
-							   'space dimension': 3,
-							   'space directions': [[dx, 0, 0], [0, dy, 0], [0, 0, dz]],
-							   'space units': ['microns', 'microns', 'microns']}
+                    options = {'type': 'uint8',
+                               'encoding': 'raw',
+                               'endian': 'big',
+                               'dimension': 3,
+                               'sizes': stack.shape,
+                               'space dimension': 3,
+                               'space directions': [[dx, 0, 0], [0, dy, 0], [0, 0, dz]],
+                               'space units': ['microns', 'microns', 'microns']}
 
-					start = time.time()
-					nrrd.write("temp_stack.nrrd", stack, options)  # save only as a temporary file
-					print("Converting Numpy Array to temp_stack NRRD " + str(time.time()-start))
+                    start = time.time()
+                    nrrd.write("temp_stack.nrrd", stack, options)  # save only as a temporary file
+                    print("Converting Numpy Array to temp_stack NRRD " + str(time.time()-start))
 
-					start = time.time()
+                    start = time.time()
 
-					readdata = apply_volume_registration_to_stack(
-						registration_files_prefix_list=[root_path / 'zbrain_registered' /
-														f'{stack_name}_time_averaged_to_Elavl3-H2BRFP'],
-						source_stack_path='temp_stack.nrrd',
-						target_stack_path=reference_brain_path / 'Elavl3-H2BRFP_z_brain.nrrd.nrrd',
-						output_stack_path='temp_stack_registered.nrrd')
+                    readdata = apply_volume_registration_to_stack(
+                        registration_files_prefix_list=[root_path / 'zbrain_registered' /
+                                                        f'{stack_name}_time_averaged_to_Elavl3-H2BRFP'],
+                        source_stack_path='temp_stack.nrrd',
+                        target_stack_path=reference_brain_path / 'Elavl3-H2BRFP_z_brain.nrrd.nrrd',
+                        output_stack_path='temp_stack_registered.nrrd')
 
-					print("apply_volume_registration_to_stack time " + str(time.time()-start))
+                    print("apply_volume_registration_to_stack time " + str(time.time()-start))
 
 
-					#start = time.time()
-					#nrrd.write("temp_stack.nrrd", stack, options)  # save only as a temporary file
-					#print("nrrd.write temp stack " + str(time.time()-start))
+                    #start = time.time()
+                    #nrrd.write("temp_stack.nrrd", stack, options)  # save only as a temporary file
+                    #print("nrrd.write temp stack " + str(time.time()-start))
 
-					# Read the registered data
-					#readdata, header = nrrd.read('temp_stack_registered.nrrd')
-					#readdata = readdata.astype(np.uint8)
+                    # Read the registered data
+                    #readdata, header = nrrd.read('temp_stack_registered.nrrd')
+                    #readdata = readdata.astype(np.uint8)
 
-					# Place it at the right place into the hdf5 file created above and bring it back to ZYX
-					
-					start = time.time()
-					dset[t, :, :, :] = readdata.transpose((2, 1, 0))
-					print("transpose data " + str(time.time()-start))
+                    # Place it at the right place into the hdf5 file created above and bring it back to ZYX
+                    
+                    start = time.time()
+                    dset[t, :, :, :] = readdata.transpose((2, 1, 0))
+                    print("transpose data " + str(time.time()-start))
 
-				f_hdf5_motion_aligned.close()
-				f_hdf5_z_brain_registered.close()
+                f_hdf5_motion_aligned.close()
+                f_hdf5_z_brain_registered.close()
